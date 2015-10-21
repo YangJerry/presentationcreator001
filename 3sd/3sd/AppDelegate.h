b49d417f57f383095de67b47a03ba56e43c760da
songyang <songyang@songyangdeair.cn.ibm.com>
2015-10-21 17:33:09 +0800

    ww

---
+++ 3sd/3sd/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  3sd
+//
+//  Created by songyang on 15/10/21.
+//  Copyright © 2015年 songyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 3sd/3sd/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  3sd
+//
+//  Created by songyang on 15/10/21.
+//  Copyright © 2015年 songyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
